802d4b2637bb2e27622e70faeee82fa6e28452ec
runCard <wfeng_work@163.com>
2022-03-25 15:45:14 +0800

    commit test code

--- app/src/main/java/com/qingfeng/test/SettingsActivity.kt
+++ app/src/main/java/com/qingfeng/test/SettingsActivity.kt
@@ -15,6 +15,7 @@ class SettingsActivity : AppCompatActivity() {
                 .replace(R.id.settings, SettingsFragment())
                 .commit()
         }
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
